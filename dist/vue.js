cd062cf2502ae0b4492c6e77d8180153a08d5f68
flytreeleft <flytreeleft@126.com>
2016-07-13 18:08:37 +0800

    build dist

--- dist/vue.js
+++ dist/vue.js
@@ -5577,8 +5577,9 @@
         name = name.replace(/^(v-bind:|:)([^\.]+).*$/, '$2');
       }
 
-      if (!disallowedMergedAttrRE.test(name) && !options[name]) {
-        options[name] = {};
+      if (!disallowedMergedAttrRE.test(name)) {
+        name = camelize(name);
+        options[name] = options[name] || {};
       }
     }
     return options;
